0af972168eedda3a07d32be64ffba2a24cf2fc7f
Emmett <emmettpeters@Emmetts-MacBook-Air.local>
2017-06-20 11:52:27 -0500

    Completed object exercises, nearly finished bonuses

--- public/js/objects.js
+++ public/js/objects.js
@@ -1,5 +1,7 @@
+"use strict";
+
 (function(){
-    "use strict";
+    
 
     // var person = {
     //     "firstName": "Emmett",
@@ -57,11 +59,241 @@
     ]; 
 
     shoppers.forEach(function(element){
-        if (amount < 200) {
-            console.log(element.name + " paid less than 200 dollars and therefore gets no discount. total is " + element.amount);
+        if (element.amount < 200) {
+                console.log(element.name + " paid less than 200 dollars and therefore gets no discount. total is " + element.amount)
+            }
+             else {
+                console.log(element.name + " paid more that 200 dollars and therefore got a discount of " + element.amount*.1 + " for a total of " + (element.amount-element.amount*.1)) 
+                //ask about having + and - versus * and divide with respect to parenthesis
+            }
+        })
+
+// todo:
+// Create an array of objects that represent books.
+// Each book should have a title and an author property.
+// The author property should be an object with a firstName and lastName.
+// Be creative and add at least 5 books to the array
+// var books = [todo];
+var books = [{
+    "title":"Harry",
+    "author":{
+        "firstName":"Dorothy",
+        "lastName":"Hamill",
+        },    
+    "keywords": ["action","adventure","scifi"],
+    "available": true,
+    "dateAvailable": new Date(),
+    rent: function () {
+        if (this.available == true)
+            {
+            this.available = false;
+            this.dateAvailable.setDate(this.dateAvailable.getDate()+14)
         } else {
-            console.log(element.name + " paid more that 200 dollars and therefore got a discount of " + (element.amount*.1) 
-                + " for a total of " + (element.amount-(element.amount*.1)))
-    })
+            alert("Book is currently rented out! Sorry =(")
+        }
+        // var currentTime = new Date();
+        // var month = currentTime.getMonth();
+        // var day = currentTime.getDay() + 14;
+        // var year = currentTime.getYear();
+        
+    },
+        
+},
+{
+    "title":"Potter",
+    "author":{
+        "firstName":"Russel",
+        "lastName":"Doug",
+        },   
+    "keywords": ["action","fiction","comedy"],
+    "available": true,
+    "dateAvailable": new Date(),
+    rent: function () {
+         this.available = false;
+        // var currentTime = new Date();
+        // var month = currentTime.getMonth();
+        // var day = currentTime.getDay() + 14;
+        // var year = currentTime.getYear();
+        this.dateAvailable = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+    },
+        
+},
+{
+    "title":"And",
+    "author":{
+        "firstName":"BK",
+        "lastName":"Hakensack",
+        },  
+    "keywords": ["action","comedy","horror"],
+    "available": true,
+    dateAvailable: new Date(),
+    rent: function () {
+         this.available = false;
+        // var currentTime = new Date();
+        // var month = currentTime.getMonth();
+        // var day = currentTime.getDay() + 14;
+        // var year = currentTime.getYear();
+        this.dateAvailable = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+    },
+        
+},
+{
+    "title":"Chamber",
+    "author":{
+        "firstName":"Lil",
+        "lastName":"Sebastian",
+        },    
+    "keywords": ["action","romantic","horror"],
+    "available": true,
+    "dateAvailable": new Date(),
+    rent: function () {
+         this.available = false;
+        // var currentTime = new Date();
+        // var month = currentTime.getMonth();
+        // var day = currentTime.getDay() + 14;
+        // var year = currentTime.getYear();
+        this.dateAvailable = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+    },
+        
+},
+{
+    "title":"Secrets",
+    "author":{
+        "firstName":"Ron",
+        "lastName":"Swanson",
+        },    
+    "keywords": ["action","non-fiction","romantic"],
+    "available": true,
+    "dateAvailable": new Date(),
+    rent: function () {
+        this.available = false;
+        // var currentTime = new Date();
+        // var month = currentTime.getMonth();
+        // var day = currentTime.getDay() + 14;
+        // var year = currentTime.getYear();
+        this.dateAvailable = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate()+14;
+    },
+        
+}]
+
+// books.forEach(function(){
+
+// })
+// log out the books array
+
+// todo:
+// Loop through the array of books using .forEach and print out the specified information about each one.
+// start loop here
+books.forEach(function(book, index, array){
+    console.log("Book #:" + parseInt(index+1));
+    console.log("Title: " + book.title);
+    console.log("Author: " + book.author.firstName + " " + book.author.lastName);
+    console.log("Keywords: " + book.keywords);
+    console.log("Available?: " + book.available);
+    console.log("Date Available: " + book.dateAvailable);
+})
+
+
+// Bonus Exercises for Objects Lesson
+
+// BONUS 1 (expanding on the books object exercise):
+
+//     Add a property “keywords” that contains an array of possibly genres the book may be categoriezed by
+//     Add a boolean property “available” and set it to true
+//     Add a dateAvailable property that has a string of the date/time when the book will be available
+//     Add a method rent() that... 
+//         - changes the available property to false if it is not already false
+//         - sets the dateAvailable to a date exactly two weeks from when the rent() method is called 
+//         (to do this, research the JS Date object and use methods from it in your code)
+//     Add a method returned() that...
+//         - changes the available property to true
+//         - changes the dateAvailble property to the string “now”
+
+
+// BONUS 2 (Create a Dog object):
+
+
+
+// var dog = {
+//     "breed": "",
+//     weightInPounds: 0,
+//     age: 0,
+//     color: "",
+//     steralized: false,
+//     shotRecords: [
+//     {
+//         date: "",
+//         typeOfShot: "",
+//     }
+//     {
+//         date: "",
+//         typeOfShot: "",
+//     }
+//     {
+//         date: "",
+//         typeOfShot: "",
+//     }
+//     ],
+//     bark: function() {
+//         console.log("Bow Wow!!!")
+//     },
+//     getOlder: function(){
+//         this.age++
+//     },
+//     fix: function(){
+//         if (this.steralized === true) {
+//             alert("Your animal is ALREADY steralized!")
+//         } else {
+//             this.steralized = !this.steralized
+//         }
+//     },
+//     getShot: function(){
+//         this.shotRecords.date.push(prompt())
+//     }
+// }
+
+//     The dog object should have properties for: 
+//         breed (string), 
+//         weightInPounds (number),
+//         age (number), 
+//         color (string), 
+//         steralized (boolean), 
+//         shotRecords (array of objects with properties for date and typeOfShot)
+
+//     The dog object should have methods to:
+//         bark() - will console.log “Woof!”
+//         getOlder() - will increase age by 1
+//         fix() - will set sterile to true
+//         getShot() - takes in an argument for the name of the shot and adds a new shot with the current date to the shotRecords array
 
 })();
+
+
+// Extra practice at night beginning individual lotto/raffle panes - practice
+// Extra practice at night beginning individual lotto/raffle panes - practice
+// Extra practice at night beginning individual lotto/raffle panes - practice
+// Extra practice at night beginning individual lotto/raffle panes - practice
+// Extra practice at night beginning individual lotto/raffle panes - practice
+
+// var twlIndividual = [
+//     {
+//         indivType: ["char","non"];
+//         indivTimeRemaining: "";
+//         indivImg: "";
+//         indivCPE: "1.00unit";
+
+
+//     }
+
+
+
+
+
+
+
+
+
+
+
+
+
