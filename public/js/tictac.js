5289110e9fc78b67b0d73592291ab0b13c1c2aa5
Emmett Peters <ejp8611@gmail.com>
2017-06-29 10:43:43 -0500

    finished up jquery class ecercise and bonus, tic tac toe functionality almost complete.

--- public/js/tictac.js
+++ public/js/tictac.js
@@ -11,27 +11,62 @@ $('.contSize').css("border","2px solid black");
 
 var i = 0;
 var boxesArray = [$('#one'),$('#two'),$('#three'),$('#four'),$('#five'),$('#six'),$('#seven'),$('#eight'),$('#nine')];
+var redArray = [];
+var blueArray = [];
 
 $('.colBord').click(function(){
 	console.log("clicked!");
-	if(i<9){
+	if(i<9 && this.style.backgroundColor == "white"){
 		if (i%2==0){
-			this.style.backgroundColor = "red";	
+			this.style.backgroundColor = "red";
+			redArray.push(Number(this.innerHTML));
+			console.log(redArray);
+
 		} else {
 			this.style.backgroundColor = "blue";
+			blueArray.push(Number(this.innerHTML));
+			console.log(blueArray);
 		};
 		i+=1;
 	}
 	console.log(i);
 });
 
-console.log($('#one').css('backgroundColor'))
 
-win=1,2,3
-	1,4,7
-	1,5,9
-	4,5,6
-	7,8,9
-	
+
+// if 
+// 	(
+// 		((blueArray.indexOf(1)!== -1) && (blueArray.indexOf(2)!== -1) && (blueArray.indexOf(3)!== -1)) ||
+// 		((blueArray.indexOf(1)!== -1) && (blueArray.indexOf(4)!== -1) && (blueArray.indexOf(7)!== -1)) ||
+// 		((blueArray.indexOf(1)!== -1) && (blueArray.indexOf(5)!== -1) && (blueArray.indexOf(9)!== -1)) ||
+// 		((blueArray.indexOf(4)!== -1) && (blueArray.indexOf(5)!== -1) && (blueArray.indexOf(6)!== -1)) ||
+// 		((blueArray.indexOf(7)!== -1) && (blueArray.indexOf(5)!== -1) && (blueArray.indexOf(3)!== -1)) ||
+// 		((blueArray.indexOf(7)!== -1) && (blueArray.indexOf(8)!== -1) && (blueArray.indexOf(9)!== -1)) ||
+// 		((blueArray.indexOf(2)!== -1) && (blueArray.indexOf(5)!== -1) && (blueArray.indexOf(8)!== -1)) ||
+// 		((blueArray.indexOf(3)!== -1) && (blueArray.indexOf(6)!== -1) && (blueArray.indexOf(9)!== -1)) ||
+// 	){}
+
+
+
+
+
+// all the logic
+// win=
+// 	1,2,3
+// 	1,4,7
+// 	1,5,9
+// 	4,5,6
+// 	7,5,3
+// 	7,8,9
+// 	2,5,8
+// 	3,6,9
+
+
+
+
+
+
+
+
 
 
