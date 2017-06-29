7b5b5941e14bc3954ad2b682c93e47cf919277b4
Emmett Peters <ejp8611@gmail.com>
2017-06-29 14:38:38 -0500

    finished jQuery traversing exercisesand completed basic js functionality of tic tac toe

--- public/js/tictac.js
+++ public/js/tictac.js
@@ -4,62 +4,69 @@
 
 $('.colBord').css({border: "1px solid black",height:"200px",backgroundColor:"white"});
 $('.contSize').css("border","2px solid black");
+$('#winner').css("height","40px");
 
 //--------------------------------------------
 //Button Functionality
 //--------------------------------------------
 
 var i = 0;
-var boxesArray = [$('#one'),$('#two'),$('#three'),$('#four'),$('#five'),$('#six'),$('#seven'),$('#eight'),$('#nine')];
 var redArray = [];
 var blueArray = [];
+var redScore = document.getElementById("redscore");
+var blueScore = document.getElementById("bluescore");
+
+$("#reset").click(function(){
+	$('.colBord').css('backgroundColor','white');
+	i=0;
+	redArray = [];
+	blueArray = [];
+	$('#winner').text("");
+});
 
 $('.colBord').click(function(){
-	console.log("clicked!");
-	if(i<9 && this.style.backgroundColor == "white"){
+	if(i<=8 && this.style.backgroundColor == "white"){
 		if (i%2==0){
 			this.style.backgroundColor = "red";
 			redArray.push(Number(this.innerHTML));
-			console.log(redArray);
+			if (
+				((redArray.indexOf(1)!== -1) && (redArray.indexOf(2)!== -1) && (redArray.indexOf(3)!== -1)) ||
+				((redArray.indexOf(1)!== -1) && (redArray.indexOf(4)!== -1) && (redArray.indexOf(7)!== -1)) ||
+				((redArray.indexOf(1)!== -1) && (redArray.indexOf(5)!== -1) && (redArray.indexOf(9)!== -1)) ||
+				((redArray.indexOf(4)!== -1) && (redArray.indexOf(5)!== -1) && (redArray.indexOf(6)!== -1)) ||
+				((redArray.indexOf(7)!== -1) && (redArray.indexOf(5)!== -1) && (redArray.indexOf(3)!== -1)) ||
+				((redArray.indexOf(7)!== -1) && (redArray.indexOf(8)!== -1) && (redArray.indexOf(9)!== -1)) ||
+				((redArray.indexOf(2)!== -1) && (redArray.indexOf(5)!== -1) && (redArray.indexOf(8)!== -1)) ||
+				((redArray.indexOf(3)!== -1) && (redArray.indexOf(6)!== -1) && (redArray.indexOf(9)!== -1))
+			){
+				redScore.innerHTML = parseInt(redScore.innerHTML) + 1;
+				i=8;
+				$('#winner').text("RED WINS!!");
+			};
 
 		} else {
 			this.style.backgroundColor = "blue";
 			blueArray.push(Number(this.innerHTML));
-			console.log(blueArray);
+			if (
+				((blueArray.indexOf(1)!== -1) && (blueArray.indexOf(2)!== -1) && (blueArray.indexOf(3)!== -1)) ||
+				((blueArray.indexOf(1)!== -1) && (blueArray.indexOf(4)!== -1) && (blueArray.indexOf(7)!== -1)) ||
+				((blueArray.indexOf(1)!== -1) && (blueArray.indexOf(5)!== -1) && (blueArray.indexOf(9)!== -1)) ||
+				((blueArray.indexOf(4)!== -1) && (blueArray.indexOf(5)!== -1) && (blueArray.indexOf(6)!== -1)) ||
+				((blueArray.indexOf(7)!== -1) && (blueArray.indexOf(5)!== -1) && (blueArray.indexOf(3)!== -1)) ||
+				((blueArray.indexOf(7)!== -1) && (blueArray.indexOf(8)!== -1) && (blueArray.indexOf(9)!== -1)) ||
+				((blueArray.indexOf(2)!== -1) && (blueArray.indexOf(5)!== -1) && (blueArray.indexOf(8)!== -1)) ||
+				((blueArray.indexOf(3)!== -1) && (blueArray.indexOf(6)!== -1) && (blueArray.indexOf(9)!== -1))
+			){
+				blueScore.innerHTML = parseInt(blueScore.innerHTML) + 1;
+				i=8;
+				$('#winner').text("BLUE WINS!!");
+			};
 		};
 		i+=1;
-	}
-	console.log(i);
+		if(i==9 && ($('winner').text()=="")){$('#winner').text("TIE!!!")};
+		console.log(i);
+	};
 });
-
-
-
-// if 
-// 	(
-// 		((blueArray.indexOf(1)!== -1) && (blueArray.indexOf(2)!== -1) && (blueArray.indexOf(3)!== -1)) ||
-// 		((blueArray.indexOf(1)!== -1) && (blueArray.indexOf(4)!== -1) && (blueArray.indexOf(7)!== -1)) ||
-// 		((blueArray.indexOf(1)!== -1) && (blueArray.indexOf(5)!== -1) && (blueArray.indexOf(9)!== -1)) ||
-// 		((blueArray.indexOf(4)!== -1) && (blueArray.indexOf(5)!== -1) && (blueArray.indexOf(6)!== -1)) ||
-// 		((blueArray.indexOf(7)!== -1) && (blueArray.indexOf(5)!== -1) && (blueArray.indexOf(3)!== -1)) ||
-// 		((blueArray.indexOf(7)!== -1) && (blueArray.indexOf(8)!== -1) && (blueArray.indexOf(9)!== -1)) ||
-// 		((blueArray.indexOf(2)!== -1) && (blueArray.indexOf(5)!== -1) && (blueArray.indexOf(8)!== -1)) ||
-// 		((blueArray.indexOf(3)!== -1) && (blueArray.indexOf(6)!== -1) && (blueArray.indexOf(9)!== -1)) ||
-// 	){}
-
-
-
-
-
-// all the logic
-// win=
-// 	1,2,3
-// 	1,4,7
-// 	1,5,9
-// 	4,5,6
-// 	7,5,3
-// 	7,8,9
-// 	2,5,8
-// 	3,6,9
 
 
 
