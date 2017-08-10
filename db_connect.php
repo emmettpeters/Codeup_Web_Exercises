1b97b7a28a1cacf83533118a19a615e18da75581
Emmett Peters <ejp8611@gmail.com>
2017-08-10 15:06:03 -0500

    added parks db for mysql and setup logins for parks

--- db_connect.php
+++ db_connect.php
@@ -2,8 +2,6 @@
 
 require_once "env.php";
 
-$dbc = new PDO("mysql:host=127.0.0.1;dbname=$db_name", $username, $password);
+$dbc = new PDO("mysql:host=" . DB_HOST . ";dbname=" . DB_NAME  , DB_USERNAME  , DB_PASSWORD);
 
 $dbc->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-echo $dbc->getAttribute(PDO::ATTR_CONNECTION_STATUS) . "\n";
