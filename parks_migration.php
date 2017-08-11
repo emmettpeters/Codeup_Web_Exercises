878886b13dfd2b298d4117947c9d65b752c561ac
Emmett Peters <ejp8611@gmail.com>
2017-08-11 16:28:08 -0500

    changed parks migration

--- parks_migration.php
+++ parks_migration.php
@@ -3,12 +3,17 @@
 require_once "park_logins.php";
 require_once "db_connect.php";
 
+// $statement = "DROP TABLE IF EXISTS national_parks";
+
+// $dbc->exec($statement);
+
 $statement = "CREATE TABLE IF NOT EXISTS national_parks (
     id INT UNSIGNED NOT NULL AUTO_INCREMENT,
     name VARCHAR(128),
     location VARCHAR(128),
     date_established DATE,
     area_in_acres DOUBLE,
+    description VARCHAR(128),
     PRIMARY KEY (id)
     );
 ";
